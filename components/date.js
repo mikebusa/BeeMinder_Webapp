f8706c1a8491a290280cfee744edbbf9e861464e
Michael Busa <busa.m@northeastern.edu>
2020-09-29 18:15:47 -0400

    Intial File Upload

--- components/date.js
+++ components/date.js
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, "'Updated: 'M/d/yyyy' at 'h:mm a")}</time>
+  return <time dateTime={dateString}>{format(date, "'Last updated: 'M/d/yyyy' at 'h:mm a")}</time>
 }
