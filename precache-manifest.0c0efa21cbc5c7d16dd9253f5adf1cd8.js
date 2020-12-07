0603acb3a84af25213570bba6ef351768576d2b0
Michael Busa <busa.m@northeastern.edu>
2020-12-07 10:33:50 -0500

    Updates

---
+++ precache-manifest.0c0efa21cbc5c7d16dd9253f5adf1cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49aedce1b94d93de7669a3643c4965bc",
+    "url": "/BeeMinder_Webapp/index.html"
+  },
+  {
+    "revision": "b8032041358a12e4e7fa",
+    "url": "/BeeMinder_Webapp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b8032041358a12e4e7fa",
+    "url": "/BeeMinder_Webapp/static/js/2.7fdb0af3.chunk.js"
+  },
+  {
+    "revision": "4a48257ff4db5aa728531d9cf913f3f5",
+    "url": "/BeeMinder_Webapp/static/js/2.7fdb0af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109830813d3efca75ea1",
+    "url": "/BeeMinder_Webapp/static/js/main.30382946.chunk.js"
+  },
+  {
+    "revision": "dacc16a7710430bcac46",
+    "url": "/BeeMinder_Webapp/static/js/runtime-main.806d50b6.js"
+  }
+]);
